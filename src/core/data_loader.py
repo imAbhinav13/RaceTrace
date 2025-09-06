6a918291051ad5aaad6c05c75444b538efc02012
Abhinav Nagathan <nagathanabhi@gmail.com>
2025-09-06 13:25:03 +0530

    Added data_loader.py with FastF1 caching

---
+++ src/core/data_loader.py
@@ -0,0 +1,15 @@
+import os
+import fastf1
+
+def enable_cache(cache_dir="src/data/cache"): 
+    # Ensure the folder exists
+    os.makedirs(cache_dir, exist_ok=True)   
+    fastf1.Cache.enable_cache(cache_dir)    
+
+def get_session(year, gp,session_type):     
+    enable_cache()                        
+    session = fastf1.get_session(year, gp, session_type) 
+    session.load()
+    return session
+
+    
